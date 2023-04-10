0555d3f589804ac36281e4a28a6d5d67e6697037
SatishK2022 <sk7399052@gmail.com>
2023-04-10 12:25:44 +0530

    Practice set 4 added

---
+++ Practice_Set/ps4.java
@@ -0,0 +1,16 @@
+package Practice_Set;
+
+public class ps4 {
+    public static void main(String[] args) {
+        // Question 1
+        // int a = 10;
+        // if(a = 12){
+        //     System.out.println("I am 12");
+        // }else{
+        //     System.out.println("I am not 12");
+        // }
+
+        // Question 2
+        
+    }
+}
